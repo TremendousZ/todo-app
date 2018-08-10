08a63d4f87058dd3b64ed204aefedd8d72658755
John Carlisle <jmcarlisle@live.com>
2018-08-09 17:06:04 -0700

    axios exercise done

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,11 @@ import React, {Component} from 'react';
 import TodoList from './todo_list';
 import AddItem from './add_item';
 import listData from '../data/todo';
+import axios from 'axios';
 
 
-
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=c618_jCarlisle';
 
 class App extends Component{
     constructor(props){
@@ -20,23 +22,46 @@ class App extends Component{
         this.getListData();
     }
 
-    addItem = (item) => {
-        item._id = new Date().getTime();
-        this.setState({
-            items:[item, ...this.state.items]
-        });
+    async addItem  (item) {
+        try{
+            if(!item.title){
+                throw new Error('Missing Title');
+            }
+            if(!item.details){
+                throw new Error('missing details');
+            }
+            
+            await axios.post(`${BASE_URL}/todos${API_KEY}`,item);
+        
+            this.getListData();
+    } catch(err){
+        console.log('something went wrong', err.message)
+    }
     }
 
-    getListData(){
-        // This is where you would call the server for data
+    async getListData(){
+        const resp = await axios.get(`${BASE_URL}/todos${API_KEY}`);
         this.setState({
-            items: listData
+            items:resp.data.todos
         });
-    }
+    }    
+    // getListData(){
+    //     // This is where you would call the server for data
+
+    //     const response = axios.get(`${BASE_URL}/todos${API_KEY}`).then((resp) =>{
+    //         this.setState({
+    //             items: resp.data.todos
+    //         });
+            
+    //     }).catch((err) => {
+    //         console.log('there was an error', err.message);
+    //     });
+    //     this.setState({
+    //         items: listData
+    //     });
+    // }
 
     render(){
-        console.log('App State: ', this.state);
-
         return (
             <div className ='container'>
                 <h1 className ='center'>To Do List</h1>
